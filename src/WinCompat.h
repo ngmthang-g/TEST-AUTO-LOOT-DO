2a2b7ea4f2c2ef1e2511bb820a2754e47be19fab
ngmthang-g <hyanrymine3@gmail.com>
2026-08-17 00:21:28 +0700

    Undef legacy Win32 min max macros

--- src/WinCompat.h
+++ src/WinCompat.h
@@ -1,9 +1,15 @@
 #pragma once
 
-// Force-include Windows headers once, then remove legacy memory-model tokens
-// such as `near` that still exist as empty macros in Win32 headers.
+// Force-include Windows headers once, then remove legacy Win32 macros that
+// collide with modern C++ identifiers/std::min/std::max.
 #include <windows.h>
 
 #ifdef near
 #undef near
+#endif
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
 #endif
